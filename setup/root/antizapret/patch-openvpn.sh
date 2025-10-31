a32e30d735d19e66323cab76675de26cfb71dd68
Даниил Селиверстов <46894057+danayer@users.noreply.github.com>
2025-10-31 12:28:20 +0800

    Update patch-openvpn.sh

--- setup/root/antizapret/patch-openvpn.sh
+++ setup/root/antizapret/patch-openvpn.sh
@@ -64,7 +64,7 @@ apt-get autoremove -y
 apt-get clean
 VERSION="$(openvpn --version | head -n 1 | awk '{print $2}')"
 mkdir -p /usr/local/src/openvpn
-curl -4fL https://build.openvpn.net/downloads/releases/openvpn-$VERSION.tar.gz -o /usr/local/src/openvpn.tar.gz
+curl -4fL https://github.com/OpenVPN/openvpn/releases/download/v2.6.14/openvpn-2.6.14.tar.gz -o /usr/local/src/openvpn.tar.gz
 tar --strip-components=1 -xvzf /usr/local/src/openvpn.tar.gz -C /usr/local/src/openvpn
 rm -f /usr/local/src/openvpn.tar.gz
 
